e963edd21a9d13f97c7344397d04909d9544bb6c
wbc <3362484152@qq.com>
2025-04-09 06:31:54 +0000

    Init arch

--- tests/test.py
+++ tests/test.py
@@ -2,6 +2,7 @@ import os
 import time
 import torch
 import torch.distributed as dist
+import deep_reduce
 
 def init_dist(local_rank: int, num_local_ranks: int):
     ip = os.getenv('MASTER_ADDR', '127.0.0.1')
@@ -20,10 +21,8 @@ def init_dist(local_rank: int, num_local_ranks: int):
     torch.set_default_device('cuda')
     torch.cuda.set_device(local_rank)
 
-    print("rank: {}, num_ranks: {}, group: {}".format(dist.get_rank(), 
-                                                      dist.get_world_size(), 
-                                                      list(range(num_local_ranks * num_nodes)))
-)
+    # print("rank: {}, num_ranks: {}, group: {}".format(dist.get_rank(), dist.get_world_size(), list(range(num_local_ranks * num_nodes))))
+
     return dist.get_rank(), dist.get_world_size(), dist.new_group(list(range(num_local_ranks * num_nodes)))
 
 def test_loop(local_rank: int, num_local_ranks: int):
@@ -31,11 +30,13 @@ def test_loop(local_rank: int, num_local_ranks: int):
     assert(rank == local_rank)
     assert(num_ranks == num_local_ranks)
 
-    test_ll_compatibility, num_rdma_bytes = False, 0
+    # test_ll_compatibility, num_rdma_bytes = False, 0
 
+    deep_reduce.torchReduce(c, a, b, n)
     # buffer = ep.Buffer(group, int(1e9), num_rdma_bytes, low_latency_mode=test_ll_compatibility,
     #                         num_qps_per_rank=(ll_num_experts // num_ranks if test_ll_compatibility else 1))
 
 if __name__ == '__main__':
     num_processes = 8
+
     torch.multiprocessing.spawn(test_loop, args=(num_processes, ), nprocs=num_processes)
